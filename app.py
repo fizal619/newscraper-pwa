b33e8d0ce1154cf6fec08eddbc1655c68c9fb428
Fizal Sarif <fizal.sarif@gmail.com>
2017-02-05 10:51:37 -0500

    working with mercury api

--- app.py
+++ app.py
@@ -1,45 +1,47 @@
 from flask import Flask
+from flask import request
 from flask import jsonify
+from flask_cors import CORS, cross_origin
+
+# from flask import json
 from bs4 import BeautifulSoup
 from newspaper import Article
 
 import os
 import requests
 
-
+# url = 'https://mercury.postlight.com/parser?url='+link.get('href')
+#         print(url)
+#         headers = {
+#           "Content-Type": "application/json",
+#           "x-api-key": "VmRQqLIvvFJXFPgsUTMkGqmVEO9li8xf0VJk5lRM"
+#         }
+#         r = requests.get(url,headers=headers)
 # HN=newspaper.build('https://news.ycombinator.com/')
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def news():
-	freshNews = []
+  freshNews = []
+  source = request.args.get('s')
+  r  = requests.get("https://newsapi.org/v1/articles?source="+source+"&sortBy=latest&apiKey=510615b5cca64c818fd652cc1888d2af")
+  data = r.json()
+  for article in data["articles"]:
+    url = 'https://mercury.postlight.com/parser?url='+article["url"]
+    headers = {
+      "Content-Type": "application/json",
+      "x-api-key": "VmRQqLIvvFJXFPgsUTMkGqmVEO9li8xf0VJk5lRM"
+      }
+    r = requests.get(url,headers=headers)
+    mercury = r.json()
+    article["content"] = mercury["content"]
 
-	r  = requests.get("https://news.ycombinator.com/")
-	data = r.text
-	soup = BeautifulSoup(data)
-
-	for link in soup.find_all('a'):
-		if len(freshNews) == 20:
-			return jsonify(freshNews)
-		try:
-			if "storylink" in link.get("class"):
-				# print(link.get("class"))
-				article = Article(link.get("href"))
-				article.download()
-				article.parse()
-				freshNews.append({
-					"title": article.title,
-					"content": article.text
-					})
-	
-
-		except Exception as e:
-			4+4
-
+  return jsonify(data["articles"])
 
 
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
