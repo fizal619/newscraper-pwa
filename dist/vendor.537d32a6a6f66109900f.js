d06a6c03cd08994f712b71e9e8ef32930f9df9fd
Fizal Sarif <fizal.sarif@gmail.com>
2017-02-21 19:34:16 -0500

    fixed mobile image

--- dist/vendor.537d32a6a6f66109900f.js
+++ dist/vendor.537d32a6a6f66109900f.js
@@ -80,7 +80,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + "cd123254b4c7aaadb00e" + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + "537d32a6a6f66109900f" + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
